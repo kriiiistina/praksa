3222a7ccd1bd7b705231340c34ff41b3599b113b
kriiiina <kristinapil002@gmail.com>
2024-03-28 14:44:35 +0100

    praksa

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    
+  });
+
+  
+
+
+
+
+  const swiper2 = new Swiper(".swiper2", {
+    // Optional parameters
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+  
